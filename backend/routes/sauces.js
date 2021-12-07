1e231646a29cf387a8c0f5816e1eee81f22d74e8
BethazRaphael <raphael.bethaz@hotmail.com>
2021-12-07 11:41:29 +0100

    back - .gitignore ajout

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -6,14 +6,12 @@ const auth = require("../middleware/auth");
 const saucesCtrl = require("../controllers/sauces");
 const multerConfig = require("../middleware/multer-config");
 
-router.post("/", auth, multer, saucesCtrl.createSauce);
+router.post("/", auth, multerConfig, saucesCtrl.createSauce);
 router.post("/:id/like", auth, saucesCtrl.likeDislikeSauce);
-router.put("/:id", auth, multer, saucesCtrl.modifySauce);
+router.put("/:id", auth, multerConfig, saucesCtrl.modifySauce);
 router.delete("/:id", auth, saucesCtrl.deleteSauce);
 router.get("/:id", auth, saucesCtrl.getOneSauce);
 router.get("/", auth, saucesCtrl.getAllSauces);
 //router.post('/:id/like', auth, saucesCtrl.addLike);
 
 module.exports = router; 
-
-module.exports = router;
