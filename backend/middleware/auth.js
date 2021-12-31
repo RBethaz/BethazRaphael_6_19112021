b0e11880726c6a081eb97f0afa47be216f885716
BethazRaphael <bethazdev@protonmail.com>
2021-12-31 08:44:31 +0100

    Ajout de commentaire

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,17 +3,18 @@ require("dotenv").config();
 
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1];
-    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN);
-    const userId = decodedToken.userId;
+    const token = req.headers.authorization.split(" ")[1]; //Récupère seulement le token du header authorization de la requête
+    const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN); //Decode le token en vérifiant le token avec celui présent dans la fonction login
+    const userId = decodedToken.userId; //Récupère le userId
     if (req.body.userId && req.body.userId !== userId) {
-      throw "Invalid user ID";
+      //Vérifie s'il y a un userId dans la requête et que celui ci est différent de l'user Id alors
+      throw "Invalid user ID"; //Renvoie l'erreur
     } else {
       next();
     }
   } catch {
     res.status(401).json({
-      error: new Error("Invalid request!")
+      error: new Error("Invalid request!"),
     });
   }
-}; 
+};
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -8,14 +8,17 @@ const MIME_TYPES = {
 };
 
 const storage = multer.diskStorage({
+  //Créé un objet de configuration pour multer pour l'enregistrer sur le disque
   destination: (req, file, callback) => {
+    //La destination d'enregistrement
     callback(null, "images");
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
+    //Nom de fichier utilisé
+    const name = file.originalname.split(" ").join("_"); //Utilise le nom d'origine en remplaçant les espaces par des undescores
+    const extension = MIME_TYPES[file.mimetype]; //Génère l'extension
+    callback(null, name + Date.now() + "." + extension); //Nom final
+  },
 });
 
-module.exports = multer({storage: storage}).single("image");
+module.exports = multer({ storage: storage }).single("image"); //méthode single pour dire que c'est un fichier unique
